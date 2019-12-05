7e68ee617c95170c94f36ff40382a2d1e2d04d40
bsjherbert <56240025+bsjherbert@users.noreply.github.com>
2019-12-05 13:38:23 -0500

    login

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,28 +4,32 @@ const router = express.Router();
 const db = require("../models");
 
 router.get("/login", (req, res) => {
-  console.log("hit this route <=============")
+  console.log("hit this route <=============");
   res.render("login");
 });
 
 router.post("/api/users", function(req, res) {
-
-  console.log("yo yo yo")
-  db.User.create(req.body).then(function(){
+  console.log("yo yo yo");
+  db.User.create(req.body).then(function() {
     res.redirect("/login");
   });
 });
 
 router.post("/login", (req, res) => {
   // Find the user whose email matches req.body.email
+  console.log(req.body);
   db.User.findOne({ where: { email: req.body.email } })
     .then(user => {
       if (!user) {
-        return res.render("login", { error: "Invalid email or password, please try again." });
+        return res.render("login", {
+          error: "Invalid email or password, please try again."
+        });
       }
 
       if (user.password !== req.body.password) {
-        return res.render("login", { error: "Invalid email or password, please try again." });
+        return res.render("login", {
+          error: "Invalid email or password, please try again."
+        });
       }
 
       req.session.userId = user.id;
