e611997a7811c791616e47a5f6edb458c826b6be
Josh Eskew <jeskew82@gmail.com>
2019-12-05 10:30:20 -0500

    redid get and post routes

--- db/schema.sql
+++ db/schema.sql
@@ -1,18 +1,6 @@
+DROP IF EXIST
+
 CREATE DATABASE exercise_db;
 
 USE exercise_db;
 
-CREATE TABLE exercise
-(
-	id int NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255),
-    type VARCHAR(255),
-    PRIMARY KEY(id)
-);
-
-CREATE TABLE input (
-    id int NOT NULL AUTO_INCREMENT,
-    sets INTEGER,
-    weights INTEGER,
-    comments VARCHAR(255)
-);
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var exphbs = require("express-handlebars");
 var express = require("express");
-var mysql = require("mysql");
-
+const db = require("./models")
 var app = express();
 
 var PORT = process.env.PORT || 8080;
@@ -14,35 +13,58 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Waynesworld13!",
-  database: "quotes_db"
-});
-
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
 
 app.get("/", function (req, res) {
-  connection.query("SELECT * FROM input;", (err, data) => {
-    if (err) throw error;
+  
+      db.Exercise.findAll().then(foundExercises => {
+        console.log(foundExercises);
+        res.render("index", { exercises: foundExercises });
+      });
+    });
+
+    app.post("/api/exercise", function(req, res) {
+      db.Exercise.create(req.body).then(function(){
+        res.redirect("/");
+      });
+    });
+  // })
+
+  // });
+  // connection.query("SELECT * FROM input;", (err, data) => {
+  //   if (err) throw error;
     // this will render the input form from the index.handlebars 
-    res.render("index", { input: data });
-  });
-});
+// });
 
-app.get("/:id", function (req, res) {
-  connection.query("SELECT * FROM input where id = ?", [req.params.id], (err, data) => {
-    if (err) throw error;
-    // Need to change out the placeholder (#) with the handlebars file path we use
-    res.render("#", data[0]);
-  });
-});
+// app.get("/:id", function (req, res) {
+//   connection.query("SELECT * FROM input where id = ?", [req.params.id], (err, data) => {
+//     if (err) throw error;
+    
+//     res.render("index", data[0]);
+//   });
+// });
 
+// app.get("/:id", function (req, res) {
+//   connection.query("SELECT * FROM input where id = ?", [req.params.id], (err, data) => {
+//     if (err) throw error
+
+//     console.log(data);
+//     res.render("index", data[0]);
+//   });
+// });
+
+// app.post("/api/input", function (req, res) {
+//   connection.query("INSERT INTO input (name, type, sets, weights, comments) VALUES (?, ?)", [req.body.name, req.body.type, req.body.sets, req.body.weights, req.body.comments], (err, data) => {
+//     if (err) throw err
+//     res.json({ id: result.insertId });
+
+//   });
+// });
+
+
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+
+});
